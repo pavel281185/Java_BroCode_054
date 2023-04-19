4cdfb205b3870bdc63dfb038cbf069cb5e5807c2
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 15:11:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(new FlowLayout(FlowLayout.RIGHT, 15, 15));
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(200, 200));
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(new FlowLayout());
+
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+
+        frame.add(panel);
+
+        frame.setVisible(true);
+    }
+}
